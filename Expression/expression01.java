ea33ab802753183dd701e24ae14fdd40cab3e8b5
mohsin0176 <mohsin2010ju@gmail.com>
2021-04-10 09:54:30 -0700

    Expression 01

---
+++ Expression/expression01.java
@@ -0,0 +1,14 @@
+class AssignmentOperator
+{
+ public static void main(String[] args) 
+ {
+
+ 	String band="mohsin";
+ 	if (band=="mohsin") 
+ 	{
+ System.out.println("Hey");		
+ System.out.println("Jude");		
+ 	}
+ 	
+ }
+}
