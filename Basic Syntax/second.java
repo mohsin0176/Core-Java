2482afc8f492fc69534f318106131a07aba5e9a2
mohsin0176 <mohsin2010ju@gmail.com>
2021-04-10 09:27:57 -0700

    Second Java Programme

---
+++ Basic Syntax/second.java
@@ -0,0 +1,22 @@
+class FreshJuice
+{
+	enum FreshJuiceSize
+	{
+		SMALL,MEDIUM,LARGE
+	}
+	FreshJuiceSize size;
+}
+
+public static void main(String[] args) 
+{
+
+public class FreshJuiceTest
+{
+	
+FreshJuice juice=new FreshJuice();
+juice.size=FreshJuice.FreshJuiceSize.MEDIUM;
+System.out.println("Size:"+juice.size);
+
+}
+	
+}
