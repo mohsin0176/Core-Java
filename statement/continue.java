48dc8281b2525559b687d4cf611d4b295fe13f63
mohsin0176 <mohsin2010ju@gmail.com>
2021-04-12 09:03:49 -0700

    Continue statement

---
+++ statement/continue.java
@@ -0,0 +1,18 @@
+public class IfExample 
+{
+	public static void main(String[] args)
+	{
+		for (int i=0;i<=20 ;i++ ) 
+		{
+			if (i==10) 
+			{
+
+				continue;
+				
+			}
+			System.out.println(i);
+			
+		}
+		
+	}
+}
