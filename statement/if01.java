98b3ac448cd4dc14b14760716918db20874dc66e
mohsin0176 <mohsin2010ju@gmail.com>
2021-04-12 08:46:06 -0700

    Second if statement

---
+++ statement/if01.java
@@ -0,0 +1,16 @@
+public class IfExample 
+{
+	public static void main(String[] args)
+	{
+		int age=20;
+		if (age%2==0) 
+		{
+			System.out.println("Age is Even");
+		} 
+		else
+		{
+			System.out.println("Age is Odd");
+		}
+		
+	}
+}
