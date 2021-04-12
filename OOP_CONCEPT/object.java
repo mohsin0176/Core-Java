aaaabfbbec0930373cd67651cf6183377d8d4c7f
mohsin0176 <mohsin2010ju@gmail.com>
2021-04-12 09:54:20 -0700

    Object of java

---
+++ OOP_CONCEPT/object.java
@@ -0,0 +1,11 @@
+public class Puppy
+{
+	public Puppy(String name)
+	{
+		System.out.println("Passed Exam");
+	}
+	public static void main(String[] args)
+	{
+		Puppy myPuppy=new Puppy("tommy");
+	}
+}
