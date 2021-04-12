1519b47d82ca8da5c3291f60266f0e2a61aa7672
mohsin0176 <mohsin2010ju@gmail.com>
2021-04-12 08:57:29 -0700

    Switch statement

---
+++ statement/switch.java
@@ -0,0 +1,29 @@
+public class IfExample 
+{
+	public static void main(String[] args)
+	{
+		int age=20;
+		switch(age)
+		{
+			case 10:
+			System.out.println("Age is Little");
+			break;
+			case 20:
+			System.out.println("Age is Not Little");
+			break;
+			case 30:
+			System.out.println("Age is Too Little");
+			break;
+			case 40:
+			System.out.println("Age is late Little");
+			break;
+			case 10:
+			System.out.println("Age is Little");
+			break;
+			default:
+			System.out.println("Age is Perfect");
+			break;
+		}
+		
+	}
+}
