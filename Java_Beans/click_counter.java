c82a01d93ada210b36ad796b6fd4e079d4785562
mohsin0176 <mohsin2010ju@gmail.com>
2021-04-13 11:16:46 -0700

    Simple Click Counter Programme

---
+++ Java_Beans/click_counter.java
@@ -0,0 +1,36 @@
+package sunw.demo.ClickCounter;
+import java.awt.*;
+import java.awt.event.*;
+
+public class ClickCounter extends Canvas
+{
+	int c;
+	public ClickCounter()
+	{
+		addMouseListener(new MouseAdapter()
+		{
+           public void mousePressed(MouseEvent me)
+           {
+           	Counter();
+           }
+		});
+		c=0;
+		setSize(100,100);
+	}
+	public void Counter()
+	{
+		c++;
+		repaint();
+	}
+	public void paint(Graphics g)
+	{
+		Dimension d=getSize();
+		int h=d.height;
+		int w=d.width;
+		g.setColor(new Color(100,100,100));
+		g.fillRect(0,0,w,h);
+		g.setColor(new Color(0,0,0));
+		g.drawString("Counter:"+c,20,50);
+	}
+}
+
