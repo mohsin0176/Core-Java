be4d1c69561f67d60a35ced15d8c0053828ca7c3
mohsin0176 <mohsin2010ju@gmail.com>
2021-04-20 11:04:00 -0700

    Create and Use Public Constant

---
+++ Var_Data/constPublic.java
@@ -0,0 +1,18 @@
+class MyConstant
+{
+
+static final int NUMBER_OF_MONTHS =12;
+static final double PI =(double)22/7;
+       
+	 
+}
+
+class Constant2
+{
+	public static void main(String args[])
+	{
+  System.out.println("NUMBER_OF_MONTHS :"+MyConstant.NUMBER_OF_MONTHS);     
+  System.out.println(" PI:"+MyConstant.PI);
+	}
+        
+}
