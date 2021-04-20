eaa96a97f3fd594fd2416c71a9213439ab362f59
mohsin0176 <mohsin2010ju@gmail.com>
2021-04-20 10:53:04 -0700

    Literal Type Integer

---
+++ Var_Data/intLiteral.java
@@ -0,0 +1,15 @@
+class Literal
+{
+	public static void main(String args[])
+	{
+        
+        long dec =1234_5678_9876_5432L;
+        int hex=0xE_23D5_8C_7;
+        int bin=0b1110_001000111010101_10001100_0111;
+        System.out.println("Decimal Number"+dec);        
+        System.out.println("Hexadec Number"+hex);
+        System.out.println("Binary Number"+bin);
+
+	}
+	 
+}
