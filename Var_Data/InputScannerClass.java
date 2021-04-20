f62403d67737cdca2d2eaa323610a4a5dc0fd8c5
mohsin0176 <mohsin2010ju@gmail.com>
2021-04-20 11:07:33 -0700

    Read data From Keyboard using Input Scanner Class

---
+++ Var_Data/InputScannerClass.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+ 
+class Input1
+{
+	public static void main(String args[])
+	{
+		Scanner sc=new Scanner(System.in);
+		int n;
+  		System.out.println(" Enter A Number:");   
+  		n=sc.nextInt();  
+ 		System.out.println(" The Given Number is"+n);
+	}
+        
+}
