3126e8c5c6ec6fed5f6fa4949759ca97356e815f
mohsin0176 <mohsin2010ju@gmail.com>
2021-04-20 08:59:38 -0700

    Static Variable Declare At the end of Class

---
+++ Var_Data/varStatic.java
@@ -0,0 +1,10 @@
+class DeclareAtEnd1
+{
+	public static void main(String args[])
+	{
+		DeclareAtEnd1 ob=new DeclareAtEnd1();
+		ob.msg="Let's learn Java";
+		System.out.println("Message:"+ob.msg);
+	}
+	String msg;
+}
