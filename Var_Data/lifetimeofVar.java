0dbafb053524e0ff1af656817ef507d8c6999598
mohsin0176 <mohsin2010ju@gmail.com>
2021-04-20 10:55:28 -0700

    Lifetime of a variable

---
+++ Var_Data/lifetimeofVar.java
@@ -0,0 +1,22 @@
+class BoolTest
+{
+	public static void main(String args[])
+	{
+        
+        int i;
+        for (i=1;i<=5 ;i++ ) 
+        {
+              increment();
+               
+        }
+
+
+	}
+       private static void increment()
+       {
+              int x=10;
+              x++;
+              System.out.println(""+x);
+       }
+	 
+}
