4b80f66af7d6aee718c423e01ab4b4456cbbdb9f
mohsin0176 <mohsin2010ju@gmail.com>
2021-04-20 11:11:11 -0700

    Read data From Keyboard using Input Console Class

---
+++ Var_Data/InputConsoleClass.java
@@ -0,0 +1,15 @@
+import java.io.*;
+ 
+
+class Input2
+{
+	public static void main(String args[])
+	{
+		Console cn=System.console();
+		int n;
+  		System.out.println("Enter A Number");     
+  	    n=Integer.parseInt(cn.readLine());
+  	    System.out.println("The Given Number:"+n); 
+	}
+        
+}
