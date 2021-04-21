84379f7825c94d9fcd18179b8e9fcb265dd3131e
mohsin0176 <mohsin2010ju@gmail.com>
2021-04-21 04:54:51 -0700

    Programme to Explain Implicit Type Conversion

---
+++ op_Expre/ImplicitConversion.java
@@ -0,0 +1,12 @@
+class TypeConversion
+{
+	public static void main(String arg[])
+	{
+           int i=2;
+           float f=13.22f;
+           byte b=1;
+           double d=124.0974;
+           double d=(i-b)*(d/f);
+            System.out.println("Answer is="+res);
+	}
+}
