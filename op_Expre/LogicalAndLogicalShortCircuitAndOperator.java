7cadb261aeb3faf17b219e57e65fc84b24e2b523
mohsin0176 <mohsin2010ju@gmail.com>
2021-04-21 04:44:14 -0700

    Logical and Logical Short Circuit And Operator

---
+++ op_Expre/LogicalAndLogicalShortCircuitAndOperator.java
@@ -0,0 +1,18 @@
+class ShortCircuitLogicalAnd
+{
+	public static void main(String arg[])
+	{
+          int a=10,b=20;
+          if ((a<5)&&(++b<10)) 
+          {
+          	System.out.println("True");
+          }
+            System.out.println("b="+b);
+
+          if ((a<5)&(++b<10)) 
+          {
+          	System.out.println("True");
+          }
+            System.out.println("b="+b);
+	}
+}
