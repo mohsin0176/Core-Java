9f488602cd7f02eb0f08766c8a8ff2d824b97d91
mohsin0176 <mohsin2010ju@gmail.com>
2021-04-21 03:54:50 -0700

    Working With Relational Opeartor

---
+++ op_Expre/RelationalOperator.java
@@ -0,0 +1,15 @@
+class ArithOpr
+{
+	public static void main(String arg[])
+	{
+		 int a=20,b=10;
+		 System.out.println("a<b="+(a<b));
+		 System.out.println("a>b="+(a>b));
+		 System.out.println("a<=b="+(a<=b));
+		 System.out.println("a>=b="+(a>=b));
+		 System.out.println("a==b="+(a==b));
+		 System.out.println("a!=b="+(a!=b));
+		 System.out.println("a="+(a));		 
+		 System.out.println("b="+(b));
+	}
+}
