cd3c083e10ae3b86a3b0fc5ac63b46611c02850b
mohsin0176 <mohsin2010ju@gmail.com>
2021-04-21 04:57:21 -0700

    Programme to Explain Implicit Type Casting

---
+++ op_Expre/TypeCasting.java
@@ -0,0 +1,11 @@
+class TypeCasting
+{
+	public static void main(String arg[])
+	{
+        int a;
+        float b=123.333f;
+        System.out.println("Before Casting"+b);
+        a=(int)b;
+        System.out.println("After Casting"+a);
+	}
+}
