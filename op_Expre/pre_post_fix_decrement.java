1338cb4e9ca84064cfa2861624d0af0e7bd527b4
mohsin0176 <mohsin2010ju@gmail.com>
2021-04-21 04:13:38 -0700

    Pre-Post Fixed Decrement Opeartor

---
+++ op_Expre/pre_post_fix_decrement.java
@@ -0,0 +1,11 @@
+class ArithOpr
+{
+	public static void main(String arg[])
+	{
+        int a=10,b=20,c;
+        c=(--a)+(--b);
+		System.out.println(" Value of a ",+a);
+		System.out.println(" Value of b ",+b);
+		System.out.println(" Value of c ",+c);
+	}
+}
