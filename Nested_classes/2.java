d80722a6c1a837ea15b7f4954e4ab0900d607271
mohsin0176 <mohsin2010ju@gmail.com>
2021-05-01 20:39:48 -0700

    Private Variable of outer class can access in inner class but private variable in inner class can not access in outer class

---
+++ Nested_classes/2.java
@@ -0,0 +1,38 @@
+class outer
+{
+	private int a;
+	Outer()
+	{
+		a=10;
+	}
+	public class Inner
+	{
+      private int b;
+      Inner()
+      {
+      	b=20;
+      }
+      void showInner()
+      {
+      	System.out.println("a="+a);
+      	System.out.println("b="+b);
+      }
+	}
+
+	void showOuter()
+	{
+		System.out.println("a="+a);
+		System.out.println("b="+b); //Error
+	}
+}
+
+class NestedCls2
+{
+	public static void main(String args[])
+	{
+		Outer otr=new Outer();
+		Outer.Inner inr=otr.new Inner();
+		otr.showOuter();
+		inr.showInner();
+	}
+}
