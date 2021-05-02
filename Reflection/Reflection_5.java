a0226874a3424fcd2a8761ec26bbb0e7b1b67d53
mohsin0176 <mohsin2010ju@gmail.com>
2021-05-01 22:08:11 -0700

    getConstructor method of Reflection in Java

---
+++ Reflection/Reflection_5.java
@@ -0,0 +1,67 @@
+import java.lang.reflect.*;
+
+class Rectangle
+{
+	private double l,b;
+    
+    Rectangle(double x,double y)
+    {
+    	l=x;
+    	b=y;
+    }
+	void area()
+	{
+		double ar=l*b;
+		System.out.println("Area of rectangle:"+ar);
+	}
+}
+
+class Triangle
+{
+	private double h,b;
+    
+    Triangle(double x,double y)
+    {
+    	b=x;
+    	h=y;
+    }
+	void area()
+	{
+		double ar=(1/2)*b*h;
+		System.out.println("Area of Triangle:"+ar);
+	}
+}
+
+ 
+class ReflectionField1
+{
+	public static void main(String[] args) 
+	{
+		try
+		{
+			Class c=Class.forName("Rectangle");
+			Field f[]=c.getDeclaredFields();
+			System.out.println("Fields Of Rectangle");
+			for (int i=0;i<f.length ;i++ ) 
+			{
+				System.out.println("Fields Of Signature"+f[i].toString());
+
+			}
+			c=Class.forName("Triangle");
+			f=c.getDeclaredFields();
+			System.out.println("Fields of Triangle");
+			for (int i=0;i<f.length ;i++ ) 
+			{
+				System.out.println("Fields Of Signature"+f[i].toString());
+
+			}
+			
+
+		}
+
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
