73d665b1bc92e81db2eab496d2e1bae13ceb36e9
mohsin0176 <mohsin2010ju@gmail.com>
2021-05-01 20:35:21 -0700

    Programme To Use Nested Class

---
+++ Nested_classes/1.java
@@ -0,0 +1,36 @@
+class outer
+{
+	private int a;
+	Outer()
+	{
+		a=10;
+	}
+	public class Inner
+	{
+      private int b;
+      Inner()
+      {
+      	b=20;
+      }
+      void showInner()
+      {
+      	System.out.println("b="+b);
+      }
+	}
+
+	void showOuter()
+	{
+		System.out.println("a="+a);
+	}
+}
+
+class NestedCls1
+{
+	public static void main(String args[])
+	{
+		Outer otr=new Outer();
+		Outer.Inner inr=otr.new Inner();
+		otr.showOuter();
+		inr.showInner();
+	}
+}
