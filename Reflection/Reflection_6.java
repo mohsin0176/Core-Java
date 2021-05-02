214c1e22a7f474190b687ce08599692f4d8566f0
mohsin0176 <mohsin2010ju@gmail.com>
2021-05-01 22:09:54 -0700

    getField method of Reflection in Java

---
+++ Reflection/Reflection_6.java
@@ -0,0 +1,46 @@
+import java.lang.reflect.*;
+
+class Rectangle
+{
+	private double l,b;
+	protected char c;
+	public int i;
+	float f;
+    
+    Rectangle(double x,double y)
+    {
+    	l=x;
+    	b=y;
+    }
+	void area()
+	{
+		double ar=l*b;
+		System.out.println("Area of rectangle:"+ar);
+	}
+}
+
+ 
+class ReflectionField0
+{
+	public static void main(String[] args) 
+	{
+		try
+		{
+			Class c=Class.forName("Rectangle");
+			Field f[]=c.getFields();
+			System.out.println("Fields Of Rectangle");
+			for (int i=0;i<f.length ;i++ ) 
+			{
+				System.out.println("Fields Of Signature"+f[i].toString());
+
+			}
+			
+
+		}
+
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
